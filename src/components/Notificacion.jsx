515eef2d4c3e4241a1654ae1883fd47f6825e54b
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-25 21:29:30 -0300

    NotificacionComponent listo y Screen con data hardcodedada de momento

---
+++ src/components/Notificacion.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {StyledText} from "../styledComponents/StyledText";
+import StyledScreenWrapper from "../styledComponents/StyledScreenWrapper";
+
+export default function Notificacion({user,msj}){
+    return(
+        <StyledScreenWrapper>
+            <View style={styles.notificacion}>
+                <Text>{user}</Text>
+
+                <Text> {msj}</Text>
+            </View>
+        </StyledScreenWrapper>
+    )
+}
+
+const styles = StyleSheet.create({
+    notificacion: {
+        flexDirection: "row",
+        paddingTop:10,
+        paddingBottom:10,
+        borderTopWidth:2,
+        borderBottomWidth: 2
+    },
+
+    texto:{
+        fontSize: 20
+    }
+})
--- src/screens/NotificacionesScreen.jsx
+++ src/screens/NotificacionesScreen.jsx
@@ -1,11 +1,64 @@
 import {StyleSheet, Text, View} from 'react-native'
 import React from 'react'
+import StyledScreenWrapper from "../styledComponents/StyledScreenWrapper";
+import Notificacion from "../components/Notificacion";
 
 export default function NotificacionesScreen() {
     return (
-        <View>
-            <Text>NotificacionesScreen</Text>
-        </View>
+        <StyledScreenWrapper>
+
+            <Notificacion
+                user={"Juan"}
+                msj={"le ha dado like a tu reseña"}
+            />
+
+            <Notificacion
+                user={"Lucas"}
+                msj={"ha comentado tu reseña"}
+            />
+
+
+            <Notificacion
+                user={"Martina"}
+                msj={"ha comenzado a seguirte"}
+            />
+
+            <Notificacion
+                user={"Sebas"}
+                msj={"le ha dado like a tu comentario"}
+            />
+
+            <Notificacion
+                user={"Selena"}
+                msj={"ha comenzado a seguirte"}
+            />
+
+            <Notificacion
+                user={"Mateo"}
+                msj={"ha comentado tu reseña"}
+            />
+
+            <Notificacion
+                user={"Valentin"}
+                msj={" le ha dado like a tu reseña"}
+            />
+
+            <Notificacion
+                user={"Mati"}
+                msj={"ha comenzado a seguirte"}
+            />
+
+            <Notificacion
+                user={"Ivo"}
+                msj={"le ha dado like a tu comentario"}
+            />
+            
+        </StyledScreenWrapper>
     )
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    contenedor:{
+        paddingTop:10
+
+    }
+})
